7cde5a463d5472fcda5a2e852e7168554fc33634
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-03-13 18:08:04 +0100

    implementata logica di vittoria del gioco

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import { useState } from "react"
 import Player from "./components/Player.jsx"
 import GameBoard from "./components/GameBoard.jsx"
 import Log from "./components/Log.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
 
 function deriveActivePlayer(gameTurns){
   let currentPlayer= 'X';
@@ -14,9 +23,36 @@ function deriveActivePlayer(gameTurns){
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  // const [hasWinner, setHasWinner] = useState('false');
   // const [activePlayer, setActivePlayer] = useState('X');
   
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  // posso capire se un giocatore ha vinto dai dati di gameTurns, senza aggiungere nuovi State o logiche sconnesse da gameTurns
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+      const {square, player} = turn;
+      const {row, col} = square;
+
+      gameBoard[row][col] = player;
+  } // -> si chiama: Deriving State
+
+  let winner = null;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol= gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol= gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol= gameBoard[combination[2].row][combination[2].column];
+
+    if(
+      firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol
+      ){
+        winner = firstSquareSymbol;
+      }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
@@ -30,7 +66,7 @@ function App() {
     });
   }
 
-  return <main>
+  return (<main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
         <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'}/>   
@@ -38,13 +74,14 @@ function App() {
         
         
       </ol>
-
+      {winner && <p>You won, {winner}!</p>}
       <GameBoard onSelectSquare={handleSelectSquare} 
-      turns={gameTurns}
+      board={gameBoard}
       />
     </div>
     <Log turns={gameTurns}/>  
   </main>
+  );
 }
 
 export default App
